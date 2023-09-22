8b7c73db2b45c88acadfb25db35845d2be4fdb1d
Nico <ndiaz413@west-mec.org>
2023-09-21 18:25:30 -0700

    Added the bin() function
    Added the bin() function to convert the decimal to binary and remove the b in the result.

--- convesion.py
+++ convesion.py
@@ -1,2 +1,5 @@
+# Stores user's decimal number in a variable
 conversionNum = int(input("What is the decimal number you wish to convert to binary? "))
-print(str(conversionNum))
+# Uses the bin() function to convert the decimal to binary
+binaryNum = print(bin(conversionNum))
+binaryNum = binaryNum.replace("b", "")
