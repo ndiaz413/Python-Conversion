daa29ea28bb895c3b0436ff5e588a4450a4cacbc
Nico <ndiaz413@west-mec.org>
2023-09-21 21:53:35 -0700

    Added Hexadecimal Conversion
    Added working conversion from decimal to hexadecimal

--- convesion.py
+++ convesion.py
@@ -1,5 +1,16 @@
+print("This program converts a decimal number to binary or hexadecimal.")
+print(" ")
 # Stores user's decimal number in a variable
-conversionNum = int(input("What is the decimal number you wish to convert to binary? "))
-# Uses the bin() function to convert the decimal to binary
-binaryNum = print(bin(conversionNum))
+bconNum = int(input("What is the decimal number you wish to convert to binary? "))
+# Converts decimal to binary & removes the "b" in the return
+binaryNum = bin(bconNum)
 binaryNum = binaryNum.replace("b", "")
+# Prints binary result
+print(str(bconNum) + " in binary is: " + str(binaryNum))
+
+# Stores user's decimal number in a variable
+hconNum = int(input("What is the decimal number you wish to convert to hexadecimal? "))
+# Converts decimal to hexadecimal
+hexaNum = hex(hconNum)
+# Prints hexadecimal result
+print(str(hconNum) + " as a hexadecimal is: " + str(hexaNum))
